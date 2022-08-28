336ee12222d76f867c1cce38005c01c8ad5ccaee
liiamarika <liia.hanninen@myy.haaga-helia.fi>
2022-08-28 22:35:04 +0300

    Tehtävä4 Ostoslista

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
+
+const [text, setText] = useState();
+const [data, setData] = useState([]);
+
+const addPressed = () => {
+  setData([...data, {key:text }]);
+  setText('');
+}
+
+const clearPressed = () => {
+  setData([])
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <View style={styles.inputContainer}> 
+      <TextInput style={{width:200, borderColor:'gray', borderWidth:1}}
+       value={text} 
+       onChangeText={text => setText(text)} />
+       </View>
+       <View style={styles.buttonContainer}>
+      <Button title="add"
+      onPress={addPressed} />
+      <Button title="clear"
+      onPress={clearPressed} />
+     </View>
+     <View style={styles.listContainer}> 
+     <Text style={{color:'blue', fontWeight:'bold'}} >Shopping list</Text>
+     <FlatList
+     data ={data} 
+     renderItem={({item}) =>
+     <Text>{item.key}</Text>}/>
+     </View>
     </View>
   );
 }
@@ -17,5 +45,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  inputContainer: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  buttonContainer: {
+    flex: 1,
+    width: 100,
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'space-around',
+  },
+  listContainer: {
+    flex: 4,
+    padding: 10,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
   },
 });
